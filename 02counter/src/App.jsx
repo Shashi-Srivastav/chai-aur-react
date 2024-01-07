eb98967144a8380d1cd4fe1afe279948c0e5c6e5
Shashi Srivastava <119119389+Shashi-Srivastav@users.noreply.github.com>
2024-01-07 07:53:52 +0000

    Fix, Assignment

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,15 +10,13 @@ function App() {
   //let counter = 15
 
   const addValue = () => {
-    //counter = counter + 1
-    setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1 )
-    setCounter(prevCounter => prevCounter + 1)
+    if(counter <20)
     setCounter(prevCounter => prevCounter + 1)
     
   }
 
   const removeValue = () => {
+    if(counter > 0)
     setCounter(counter - 1)
   }
   
